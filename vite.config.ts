41286576819ba93b4cffd4071aec57fabf1fa500
Voloshun <94966717+Voloshun555@users.noreply.github.com>
2026-01-28 17:58:23 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
+  base: '/Unlock-the-Wonders-of-Ukraine/',
   plugins: [injectHTML()],
 });
