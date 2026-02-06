9325d89c77c8e2562aacdf2549715f43c3843a63
Voloshun <94966717+Voloshun555@users.noreply.github.com>
2026-02-06 11:53:46 +0200

    data reviews addet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import "./ts/data-our-tours.ts";
 import "./ts/render-our-tours.ts";
-import "./ts/data-gallery.ts";
 import "./ts/renderGallery.ts";
 import "./ts/slider.ts";
---
+++ src/ts/data-reviews.ts
@@ -0,0 +1,83 @@
+import imgDmitro from "/image/reviews/Dmitro.png";
+import imgDmitroX from "/image/reviews/Dmitro@2x.png";
+import imgAlisa from "/image/reviews/Alisa.png";
+import imgAlisaX from "/image/reviews/Alisa@2x.png";
+import imgMarry from "/image/reviews/Marry.png";
+import imgMarryX from "/image/reviews/Marry@2x.png";
+import imgFedir from "/image/reviews/Fedir.png";
+import imgFedirX from "/image/reviews/Fedir@2x.png";
+import imgAnastasia from "/image/reviews/Anastasia.png";
+import imgAnastasiaX from "/image/reviews/Anastasia@2x.png";
+import imgVictoria from "/image/reviews/Victoria.png";
+import imgVictoriaX from "/image/reviews/Victoria@2x.png";
+import imgValeriy from "/image/reviews/Valeriy.png";
+import imgValeriyX from "/image/reviews/Valeriy@2x.png";
+import imgIlona from "/image/reviews/Ilona.png";
+import imgIlonaX from "/image/reviews/Ilona@2x.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "Dmitro",
+    text: "It was an incredible trip and I am very grateful to the organizers for the opportunity to learn more about our country",
+    img: imgDmitro,
+    imgX2: imgDmitroX,
+    star: 5,
+  },
+  {
+    id: 2,
+    name: "Alisa",
+    text: "I really like to travel and try to do it as often as possible. The last one was to Lake Synevir and it was very cool, a great route, a comfortable bus, delicious food. I will definitely go somewhere else with you",
+    img: imgAlisa,
+    imgX2: imgAlisaX,
+    star: 5,
+  },
+  {
+    id: 3,
+    name: "Marry",
+    text: "Great tour. I really liked the atmosphere, professional approach and very interesting information",
+    img: imgMarry,
+    imgX2: imgMarryX,
+    star: 5,
+  },
+  {
+    id: 4,
+    name: "Fedir",
+    text: "We traveled with a company and it was incredible, everyone without exception was satisfied and we will definitely go somewhere else with you and will recommend it to everyone",
+    img: imgFedir,
+    imgX2: imgFedirX,
+    star: 5,
+  },
+  {
+    id: 5,
+    name: "Anastasia",
+    text: "Professional guides, comfortable transport, excellent hotels and exciting excursions - it was unforgettable! I recommend this company to anyone who wants to get the best experience out of their trip.",
+    img: imgAnastasia,
+    imgX2: imgAnastasiaX,
+    star: 5,
+  },
+  {
+    id: 6,
+    name: "Victoria",
+    text: "I felt cozy and comfortable throughout the tour. Would have liked a little more free time to explore on my own, but overall was happy with the tour.",
+    img: imgVictoria,
+    imgX2: imgVictoriaX,
+    star: 5,
+  },
+  {
+    id: 7,
+    name: "Valeriy",
+    text: "My trip on the tour with this site was unforgettable! Excellent organization, comfortable hotels and exciting excursions. Every detail was carefully thought out and I could enjoy every moment without any worries.",
+    img: imgValeriy,
+    imgX2: imgValeriyX,
+    star: 5,
+  },
+  {
+    id: 7,
+    name: "Ilona",
+    text: "My tour with this agency was incredible! Everything was organized to a high standard, including transport, accommodation and excursions. The guides were very professional and knew every corner of the place where we were going.",
+    img: imgIlona,
+    imgX2: imgIlonaX,
+    star: 5,
+  },
+];
