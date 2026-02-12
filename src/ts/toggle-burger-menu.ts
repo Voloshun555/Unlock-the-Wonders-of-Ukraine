675226d5ec330e75159f052af72f33411c49c822
Voloshun <94966717+Voloshun555@users.noreply.github.com>
2026-02-12 16:26:23 +0200

    adit burger menu

--- src/ts/toggle-burger-menu.ts
+++ src/ts/toggle-burger-menu.ts
@@ -1,14 +1,29 @@
-const burgerBtn = document.querySelector(".btn__burger-menu");
-const iconOpen = document.querySelector(".open-burger-menu");
-const iconClose = document.querySelector(".close-burger-menu");
-const burgerMenu = document.querySelector(".burger-menu");
+const burgerBtn = document.querySelector(
+  ".btn__burger-menu",
+) as HTMLButtonElement | null;
+const iconOpen = document.querySelector(
+  ".open-burger-menu",
+) as HTMLElement | null;
+const iconClose = document.querySelector(
+  ".close-burger-menu",
+) as HTMLElement | null;
+const burgerMenu = document.querySelector(".burger-menu") as HTMLElement | null;
 const body = document.body;
 
 function toggleBurgerMenu(): void {
   burgerMenu?.classList.toggle("is-open");
   iconOpen?.classList.toggle("is-hidden");
   iconClose?.classList.toggle("is-hidden");
-  body.classList.toggle("modal-open")
+  body.classList.toggle("modal-open");
 }
 
 burgerBtn?.addEventListener("click", toggleBurgerMenu);
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 834) {
+    burgerMenu?.classList.remove("is-open");
+    iconOpen?.classList.remove("is-hidden");
+    iconClose?.classList.add("is-hidden");
+    body.classList.remove("modal-open");
+  }
+});
