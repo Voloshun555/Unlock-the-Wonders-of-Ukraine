2ffd4aa3f17d5e8018a6194c2b1a65d1bd2f5c57
Voloshun <94966717+Voloshun555@users.noreply.github.com>
2026-02-24 18:51:00 +0200

    add responsive gallery and ourTours

--- src/ts/slider.ts
+++ src/ts/slider.ts
@@ -5,8 +5,20 @@ import "swiper/css";
 function initSliders() {
   const sliderContainers = document.querySelectorAll(".js-slider-container");
 
+  let slidesCount = 1;
+  let slidesOnTablet = 1;
+
   sliderContainers.forEach((container) => {
     const swiperEl = container.querySelector(".swiper") as HTMLElement;
+    if (!swiperEl) return;
+    const gallery = swiperEl.classList.contains("swiper-container-gallery");
+    const reviews = swiperEl.classList.contains("swiper-container-reviews");
+    const ourTours = swiperEl.classList.contains("swiper-container-tours");
+
+    if (gallery) {
+      slidesOnTablet = 2;
+    }
+
     const nextBtn = container.querySelector(
       ".swiper-button-next",
     ) as HTMLButtonElement;
@@ -18,9 +30,14 @@ function initSliders() {
       new Swiper(swiperEl, {
         modules: [Navigation, Pagination],
         direction: "horizontal",
-        slidesPerView: 1,
+        slidesPerView: slidesCount,
         spaceBetween: 20,
-
+        breakpoints: {
+          835: {
+            slidesPerView: slidesOnTablet,
+            spaceBetween: 16
+          },
+        },
         navigation: {
           nextEl: nextBtn,
           prevEl: prevBtn,
